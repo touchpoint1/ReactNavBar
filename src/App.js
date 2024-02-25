f38ecfdd41ea8bb3b73dfc3a60cab08540fd40d5
MB-Darwin <133462358+MB-Darwin@users.noreply.github.com>
2024-02-25 21:40:17 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
-import Footer from "./components/Footer";
-import Header from "./components/Header";
+import { useEffect } from "react";
+import { Navigate, Outlet, Route, Routes, useLocation } from "react-router-dom";
+import { ClientLayout } from "layouts";
+import routes from "routes";
+import Home from "views/home";
 
 function App() {
+  const location = useLocation();
+  let layout;
+
+  useEffect(() => {
+    routes.forEach((route) =>
+      location.pathname.includes(route.path)
+        ? (document.title = `TouchPoint: ${route.name}`)
+        : null
+    );
+  }, [location]);
+
+  const getRoutes = (routes, layout) => {
+    console.log(layout);
+    return routes.map((route, i) => {
+      if (route.layout === layout) {
+        return <Route path={route.path} element={route.component} key={i} />;
+      }
+
+      return null;
+    });
+  };
+
   return (
-    <div className="App">
-      <Header />
-      <div className="bg-gray-200 w-full h-screen"></div>
-      <Footer />
-    </div>
-   
+    <Routes>
+      <Route path="/" element={<Navigate to={routes[0].path} />} />
+      <Route
+        path="/"
+        element={
+          <ClientLayout>
+            <Outlet />
+          </ClientLayout>
+        }
+      >
+        {getRoutes(routes, (layout = "client"))}
+      </Route>
+    </Routes>
   );
 }
 
